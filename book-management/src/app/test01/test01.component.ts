d6fb2a4789bf9f4a656428707739232c6c6d6528
Deepu Cyriac <deepucvk123@gmail.com>
2025-01-07 19:07:41 +0530

    added test component

---
+++ book-management/src/app/test01/test01.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test01',
+  imports: [],
+  templateUrl: './test01.component.html',
+  styleUrl: './test01.component.css'
+})
+export class Test01Component {
+
+}
