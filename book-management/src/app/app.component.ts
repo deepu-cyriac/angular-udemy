ecdb14255313f94b06afb5a1659574001cc1ce20
Deepu Cyriac <deepucvk123@gmail.com>
2025-01-08 09:09:30 +0530

    testing new component

--- book-management/src/app/app.component.ts
+++ book-management/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Test01Component } from './test01/test01.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Test01Component],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'book-management';
